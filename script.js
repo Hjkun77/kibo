28771511cbb1f2faec5bf10ce1af8df88dd00d08
Harvey Jay Sison <38162086+Hjkun77@users.noreply.github.com>
2019-04-10 11:49:52 +0800

    release 1.0.0 (#6)
    * home, about and application done
    
    * fix
    
    * kibo
    
    * refactor(styles): style.css fix
    
    * Added pages for Polka and Ba-Ingles (#1)
    
    * update
    
    * ba-ingles image carousel
    
    * iframe: youtube embded dances
    
    * polka carousel finish
    
    * Added first section for About and last section for Home (#2)
    
    * Iris (#3)
    
    * Added first section for About and last section for Home
    
    * Updated illustrations
    
    * fix
    
    * fix picture
    
    * Application Youtube Video (#4)
    
    * polka finish
    
    * Ba Ingles Hover
    
    * fix hover state
    
    * application.html
    
    * GIFS changed
    
    * changes
    
    * animation + logo
    
    * Adaptation
    
    * application fixes (#5)
    
    * hover fix

---
+++ script.js
@@ -0,0 +1,163 @@
+const BaIngles1 = document.querySelector('#baIngles-1')
+const BaIngles2 = document.querySelector('#baIngles-2')
+const BaIngles3 = document.querySelector('#baIngles-3')
+const BaIngles4 = document.querySelector('#baIngles-4')
+const BaIngles5 = document.querySelector('#baIngles-5')
+const BaIngles6 = document.querySelector('#baIngles-6')
+const BaIngles7 = document.querySelector('#baIngles-7')
+const BaIngles8 = document.querySelector('#baIngles-8')
+const BaIngles9 = document.querySelector('#baIngles-9')
+
+const Polka1 = document.querySelector('#polka1')
+const Polka2 = document.querySelector('#polka2')
+const Polka3 = document.querySelector('#polka3')
+const Polka4 = document.querySelector('#polka4')
+const Polka5 = document.querySelector('#polka5')
+
+function polka1() {
+  Polka1.style.display = "flex"
+  Polka2.style.display = "none"
+  Polka3.style.display = "none"
+  Polka4.style.display = "none"
+  Polka5.style.display = "none"
+}
+
+function polka2() {
+  Polka1.style.display = "none"
+  Polka2.style.display = "flex"
+  Polka3.style.display = "none"
+  Polka4.style.display = "none"
+  Polka5.style.display = "none"
+}
+
+function polka3() {
+  Polka1.style.display = "none"
+  Polka2.style.display = "none"
+  Polka3.style.display = "flex"
+  Polka4.style.display = "none"
+  Polka5.style.display = "none"
+}
+
+function polka4() {
+  Polka1.style.display = "none"
+  Polka2.style.display = "none"
+  Polka3.style.display = "none"
+  Polka4.style.display = "flex"
+  Polka5.style.display = "none"
+}
+
+function polka5() {
+  Polka1.style.display = "none"
+  Polka2.style.display = "none"
+  Polka3.style.display = "none"
+  Polka4.style.display = "none"
+  Polka5.style.display = "flex"
+}
+
+function baIngles1() {
+  BaIngles1.style.display = 'flex'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles2() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'flex'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles3() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'flex'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles4() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'flex'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles5() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'flex'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles6() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'flex'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles7() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'flex'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles8() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'flex'
+  BaIngles9.style.display = 'none'
+}
+
+function baIngles9() {
+  BaIngles1.style.display = 'none'
+  BaIngles2.style.display = 'none'
+  BaIngles3.style.display = 'none'
+  BaIngles4.style.display = 'none'
+  BaIngles5.style.display = 'none'
+  BaIngles6.style.display = 'none'
+  BaIngles7.style.display = 'none'
+  BaIngles8.style.display = 'none'
+  BaIngles9.style.display = 'flex'
+}
